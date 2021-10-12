688bb34af0eb14d24497dbda943bf1b32de2b1b6
George Boot <884482+georgeboot@users.noreply.github.com>
2021-10-12 17:04:36 +0200

    Worker starting to take shape

--- backend/worker.js
+++ backend/worker.js
@@ -1,10 +1,23 @@
+import { Router } from 'itty-router'
+import { error, json } from 'itty-router-extras'
+
+const router = Router()
+
+router.get('/', (request, env) => {
+  // now have access to the env (where CF bindings like durables, KV, etc now are)
+
+  return new Response('Hi there!')
+})
+
+router.post('/scan', async (request, env) => {
+    const content = await request.json()
+    const code = content.code
+
+    return code ? json({ success: true, code }) : error(400, 'Validation error')
+})
+
+router.all('*', () => error(404, { error: 'Page not found.' }))
+
 export default {
-    // * request is the same as `event.request` from the service worker format
-    // * waitUntil() and passThroughOnException() are accessible from `ctx` instead of `event` from the service worker format
-    // * env is where bindings like KV namespaces, Durable Object namespaces, Config variables, and Secrets
-    // are exposed, instead of them being placed in global scope.
-    async fetch(request, env, ctx) {
-        const headers = { 'Content-Type': 'text/html;charset=UTF-8' }
-        return new Response('foo bar', { headers })
-    }
+    fetch: router.handle,
 }
